30d07cf3439894e501a600d9dc8cdba3561c0bfe
James McGrogan <jmcgroga@gmail.com>
2023-12-06 20:46:17 -0500

    Updated README.md with update instructions. Updated test_mlc.py to use model for arm64 or x86_64.

--- README.md
+++ README.md
@@ -14,6 +14,17 @@ git clone https://huggingface.co/mlc-ai/mlc-chat-Llama-2-7b-chat-hf-q4f16_1 \
 git clone https://github.com/mlc-ai/binary-mlc-llm-libs.git dist/prebuilt/lib
 ```
 
+## Updating MLC and Model
+```shell
+python3 -m pip install --pre -U -f https://mlc.ai/wheels mlc-chat-nightly mlc-ai-nightly && \
+pushd dist/prebuilt/lib && \
+git pull && \
+popd && \
+pushd dist/prebuilt/mlc-chat-Llama-2-7b-chat-hf-q4f16_1 && \
+git pull && \
+popd
+```
+
 ## Running Tests
 
 ```python
--- test_mlc.py
+++ test_mlc.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from mlc_chat import ChatModule
@@ -6,8 +7,18 @@ from mlc_chat.callback import StreamToStdout, StreamIterator
 from threading import Thread
 
 class TestMLC(unittest.TestCase):
+    def get_llama_model(self):
+        arch = os.uname().machine
+        if arch == 'x86_64':
+            return "Llama-2-7b-chat-hf-q4f16_1-metal_x86_64"
+        elif arch == 'arm64':
+            return "Llama-2-7b-chat-hf-q4f16_1"
+        else:
+            raise Exception(f'Unknown architecture: {arch}')
+    
     def test_llama(self):
-        cm = ChatModule(model="Llama-2-7b-chat-hf-q4f16_1")
+        model = self.get_llama_model()
+        cm = ChatModule(model=model)
         
         prompt = "What is the meaning of life?"
         
